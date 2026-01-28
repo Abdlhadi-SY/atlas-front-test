98d042b2f341f98c701021bbab91339a3150df86
Abdlhade_SY <abdlhadesy8@gmail.com>
2026-01-28 01:38:34 -0800

    upadte files

--- src/Pages/Updateitem.jsx
+++ src/Pages/Updateitem.jsx
@@ -1,11 +1,5 @@
-import { useContext, useEffect, useState } from "react";
-import { Modback } from "../Context/ItemContext";
 import Itemform from "../Component/Itemform";
-import axios from "axios";
-import Cookies from "universal-cookie";
-import {baseUrl} from "../Variables";
+
 export default function Updateitem({item}){
-    const cookie=new Cookies();
-    const token=cookie.get("Bearer");
     return(<Itemform header={"تعديل مهمة "} content={"تعديل"} item={item}></Itemform>);   
 }
